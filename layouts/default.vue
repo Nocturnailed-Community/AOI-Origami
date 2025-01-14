efbbcc5b8bf2eb83e65c15e1c7303c2c912ff5d6
milal <milalipan13@gmail.com>
2025-01-14 13:32:46 +0700

    landing page origami

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <Header />
+        <slot />
+        <Footer />
+    </section>
+</template>
+
+<script>
+import Header from '~/components/baseComponent/headerOrigami/header.vue';
+import Footer from '~/components/baseComponent/footerOrigami/footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
